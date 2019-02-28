e33ebef4a6507df19f3d8ac9580cbe28c504dbe6
tsmithe2 <thomas_smithey@yahoo.com>
2019-02-28 09:56:48 -0600

    finished the css manipulation exercise

--- cssManipulation.js
+++ cssManipulation.js
@@ -1,7 +1,22 @@
 function changeColor()
 {
-    let border_ = document.getElementById("border").value;
-    let bg_color = document.getElementById("bgcolor").value;
-
+    /*let para_ = document.getElementById("para"); this worked!
+    para_.style.backgroundColor = "red";*/
     
+    let b_Red = document.getElementById("b_redValue").value;
+    let b_Green = document.getElementById("b_greenValue").value;
+    let b_Blue = document.getElementById("b_blueValue").value;
+    let b_Width = document.getElementById("b_width").value;
+
+    let bg_Red = document.getElementById("bg_redValue").value;
+    let bg_Green = document.getElementById("bg_greenValue").value;
+    let bg_Blue = document.getElementById("bg_blueValue").value;
+
+    b_Color = "rgb(" + b_Red + "," + b_Green + "," + b_Blue + ")";
+    bg_Color = "rgb(" + bg_Red + "," + bg_Green + "," + bg_Blue + ")";
+
+    let para_ = document.getElementById("para");
+
+    para_.style.border = b_Width + "px solid " + b_Color;
+    para_.style.backgroundColor = bg_Color;
 }
